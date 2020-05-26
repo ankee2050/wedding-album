bbfcced6b03a00252cf543ad736385b1347b45b8
ankee2050 <ankitp571@gmail.com>
2020-05-26 12:41:46 +0530

    add image resize functionality

--- album/models.py
+++ album/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 class Photos(models.Model):
 	photo_name = models.CharField(max_length=100)
@@ -16,6 +20,19 @@ class Comments(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def save(self):
+		basewidth = 100
+		im = Image.open(self.profile_picture)
+		im = im.convert('RGB')
+		output = BytesIO()
+		wpercentage = (basewidth/float(im.size[0]))
+		hsize = int(float(im.size[1]*float(wpercentage)))
+		im = im.resize((basewidth,hsize))
+		im.save(output, format='JPEG', quality=90)
+		output.seek(0)
+		self.profile_picture = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.profile_picture.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+		super(Comments, self).save()
 
 class Wish(models.Model):
 	name = models.CharField(max_length=100)
